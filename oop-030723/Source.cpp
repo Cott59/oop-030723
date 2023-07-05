edab1ad76e930526c061ab1cd4d949c6c149e858
Kto_I_takoi <ponosyzbeka99@gmail.com>
2023-07-05 21:28:16 +0300

    gg

--- oop-030723/Source.cpp
+++ oop-030723/Source.cpp
@@ -1,47 +1,78 @@
 #include<iostream>
 
-class Plant:public Harvest {
+class Plant {
 public:
-	
-	Harvest ColorHarvest(Harvest){
-		_ColorHarvest = Harvest::ColorHarvest();
+	Plant(const std::string SizePlant, const std::string ColorPlant, int number) {
+		_SizePlant = SizePlant;
+		_ColorPlant = ColorPlant;
+		_Number_of_Fruits = number;
 	}
 
-	void SizeHarvest() override {}
+	void PlantInfo() {
+		std::cout << "SizePlant: " << _SizePlant << "\n";
+		std::cout << "ColorPlant: " << _ColorPlant << "\n";
+		std::cout << "Number_of_Fruits: "<<_Number_of_Fruits << "\n";
+
+	}
+	virtual Harvest harvest() = 0;
+		
+	int Get_Number_of_Fruits() {
+		return _Number_of_Fruits;
+	}
+	void Set_Number_of_Fruits(int num) {
+		_Number_of_Fruits = num;
+	}
+
 
 private:
-	std::string _SizeHarvest;
-	std::string _ColorHarvest;
+	std::string _SizePlant="";
+	std::string _ColorPlant="";
+	int _Number_of_Fruits = 0;
 
 };
 
-class Harvest {
+class Harvest : public Plant {
 public:
-	Harvest(std::string name) :_Name(name){}
-	Harvest(std::string name, double weight) :Harvest(name) {
-		_Weight = weight;
+	Harvest(const std::string SizePlant, const std::string ColorPlant, int number, const std::string name) : Plant(SizePlant, ColorPlant, number), _NameFruits(name) {}
+	Harvest(const std::string name) : _NameFruits(name){}
+
+
+	void SetWeight(double num) {
+		_Weight = num;
 	}
-	Harvest(std::string name, double weight, std::string color) : Harvest(name,weight) {
+	void SetColor(std::string color) {
 		_ColorHarvest = color;
 	}
 
-	virtual void SizeHarvest() {
-		if (_Weight < 100)
-			_SizeHarvest = "small";
-		if (_Weight >= 100 && _Weight <= 500)
-			_SizeHarvest = "middle";
-		if (_Weight > 500)
-			_SizeHarvest = "big";
+	Harvest *harvest() override{
+		int NumberFruits = Plant::Get_Number_of_Fruits();
+		--NumberFruits;
+		Plant::Set_Number_of_Fruits(NumberFruits);
+		return new harvest("Red")
 	}
 
-	std::string ColorHarvest() {
-		return _ColorHarvest;
+	~Harvest()
+	{
+
+	}
+
+	void HarvestInfo()const {
+		std::cout << "NameFruits: " << _NameFruits << "\n";
+		std::cout << "Weight: " << _Weight << "\n";
+		std::cout << "ColorHarvest: " << _ColorHarvest << "\n";
+
 	}
 
 private:
-	std::string _Name;
+	std::string _NameFruits = "";
 	double _Weight = 0;
-	std::string _ColorHarvest = "black";
-	std::string _SizeHarvest;
+	std::string _ColorHarvest = "";
+
 
 };
+
+
+int main() {
+
+	return 0;
+}
